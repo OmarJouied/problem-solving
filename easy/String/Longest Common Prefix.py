e87a1449dbc8b73a9520c3b9e02d2337c69fa31b
OmarJouied <omarjouied1421@yahoo.com>
2026-01-31 21:48:04 +0100

    Solving the problem of finding the longest common prefix string amongst an array of strings.

---
+++ easy/String/Longest Common Prefix.py
@@ -0,0 +1,28 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        """
+        Function to find the longest common prefix string amongst an array of strings.
+        
+        :param strs: list of words
+        :type strs: list[str]
+        :return: the longest common prefix string
+        :rtype: str
+        """
+        longest = ""
+        base_word = strs[0]
+
+        idx = 0
+        while idx < len(base_word):
+          
+          for word in strs[1:]:
+            if idx >= len(word) or base_word[idx] != word[idx]:
+              return longest
+            
+          longest += base_word[idx]
+
+          idx += 1
+        
+        return longest
+  
+solution = Solution().longestCommonPrefix(["ab", "a"])
+print(solution)
