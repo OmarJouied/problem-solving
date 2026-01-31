09e6d23a3c7993547fc3ce9ff1d85b6f737f52b3
OmarJouied <omarjouied1421@yahoo.com>
2026-02-01 00:51:33 +0100

    Solving the problem of cheking if a string contains opening and closing brackets are valid

---
+++ easy/String/Valid Parentheses.py
@@ -0,0 +1,53 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        """
+        Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+        An input string is valid if:
+
+        Open brackets must be closed by the same type of brackets.
+        Open brackets must be closed in the correct order.
+        Every close bracket has a corresponding open bracket of the same type.
+        
+        :param s: the string input parentheses.
+        :type s: str
+        :return: if the input string is valid or not.
+        :rtype: bool
+        """""
+        opening_count = 0
+        prev_bracket = ""
+        idx = 0
+
+        while idx < len(s):
+          if s[idx] == '[':
+            opening_count += 1
+          elif s[idx] == '(':
+            opening_count += 1
+          elif s[idx] == '{':
+            opening_count += 1
+          elif s[idx] == ']':
+            if prev_bracket == '[':
+              opening_count -= 1
+              s = s[0: idx - 1] + (s[idx + 1:] if idx + 1 < len(s) else "")
+              idx -= 2
+            else:
+              opening_count += 1
+          elif s[idx] == ')':
+            if prev_bracket == '(':
+              opening_count -= 1
+              s = s[0: idx - 1] + (s[idx + 1:] if idx + 1 < len(s) else "")
+              idx -= 2
+            else:
+              opening_count += 1
+          elif s[idx] == '}':
+            if prev_bracket == '{':
+              opening_count -= 1
+              s = s[0: idx - 1] + (s[idx + 1:] if idx + 1 < len(s) else "")
+              idx -= 2
+            else:
+              opening_count += 1
+          
+          prev_bracket = s[idx] if idx >= 0 else ""
+          idx += 1
+        
+        return False if opening_count else True
